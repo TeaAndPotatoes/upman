c8873887c22dd6e961542f86b23c9398a028f8df
Brendan Doney <bre.doney@gmail.com>
2018-03-21 17:51:28 -0400

    Created Add Command

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate clap;
 
-#[allow(unused_imports)]
 use clap::{App, Arg, SubCommand};
 use std::io::{BufRead, BufReader};
-// use std::io::prelude::*;
-use std::fs::{OpenOptions, File};
+use std::io::prelude::*;
+use std::fs::{File, OpenOptions};
 
 fn main() {
     let app = create_app();
@@ -12,17 +11,26 @@ fn main() {
 
     let config_filename = "foo.txt";
 
-    let config_file = OpenOptions::new()
+    let mut config_file = OpenOptions::new()
         .read(true)
         .write(true)
+        .append(true)
         .create(true)
         .open(config_filename)
         .expect("Unable to open or create file at current directory");
 
     match matches.subcommand_name() {
-        Some("list") => {
-            println!("List was used");
-            list_tools(config_file);
+        Some("list") => list_tools(&config_file),
+        Some("add") => {
+            // The value of input is required and add has been confirmed, so `input` can be parsed
+            config_file
+                .write_all(
+                    format!("{}\n\n",
+                        matches.subcommand_matches("add").unwrap()
+                            .value_of("input").unwrap()
+                    ).as_bytes(),
+                )
+                .expect("Unable to write to file");
         }
         None => println!("No subcommand was run"),
         _ => println!("An unchecked subcommand was run"),
@@ -36,14 +44,25 @@ fn create_app() -> App<'static, 'static> {
         .about("Cross-platform, command-line update manager for easily checking and scheduling updates for different tools")
         .subcommand(SubCommand::with_name("list")
             .about("List all of the update commands added to this tool")
+        )
+        .subcommand(SubCommand::with_name("add")
+            .about("Used for adding a command to check for updates")
+            .arg(Arg::with_name("input")
+                .required(true)
+                .takes_value(true)
+                .help("the command to add to the update checker")
+            )
         );
 }
 
-fn list_tools(file: File) {
+fn list_tools(file: &File) {
     println!("Commands are not currently scheduled to check at any regular period\n");
+    println!("Registered commands:");
     let buf_reader = BufReader::new(file);
     for line in buf_reader.lines() {
         let l = line.expect("Could not read line from file");
-        println!("\t{}", l);
+        if !l.is_empty() {
+            println!("\t{}", l);
+        }
     }
 }
