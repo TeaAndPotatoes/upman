e3b33de0956dc9082a51569323df3c074bc997a7
Brendan Doney <bre.doney@gmail.com>
2018-03-21 22:17:10 -0400

    Run commands from Config File

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::{App, Arg, SubCommand};
 use std::io::{BufRead, BufReader};
 use std::io::prelude::*;
 use std::fs::{File, OpenOptions};
+use std::process::Command;
 
 fn main() {
     let app = create_app();
@@ -21,6 +22,7 @@ fn main() {
 
     match matches.subcommand_name() {
         Some("list") => list_tools(&config_file),
+        Some("run") => run_updates(&config_file),
         Some("add") => {
             // The value of input is required and add has been confirmed, so `input` can be parsed
             config_file
@@ -45,8 +47,11 @@ fn create_app() -> App<'static, 'static> {
         .subcommand(SubCommand::with_name("list")
             .about("List all of the update commands added to this tool")
         )
+        .subcommand(SubCommand::with_name("run")
+            .about("Run each of the commands stored through add")
+        )
         .subcommand(SubCommand::with_name("add")
-            .about("Used for adding a command to check for updates")
+            .about("Add a command to the list of tools to update")
             .arg(Arg::with_name("input")
                 .required(true)
                 .takes_value(true)
@@ -63,6 +68,24 @@ fn list_tools(file: &File) {
         let l = line.expect("Could not read line from file");
         if !l.is_empty() {
             println!("\t{}", l);
+        }
+    }
+}
+
+fn run_updates(file: &File) {
+    let buf_reader = BufReader::new(file);
+    for line in buf_reader.lines() {
+        let l = line.expect("Could not read line from file");
+        if !l.is_empty() {
+            let mut collection: Vec<&str> = l.split(" ").collect();
+            let mut command = Command::new(collection.remove(0)); // First part is always assured when splitting
+            for part in collection {
+                command.arg(part); // Adding additional args from Vec, if any
+            }
+            let output = command.output().expect("Failed to execute process");
+            println!("status: {}", output.status);
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
         }
     }
 }
